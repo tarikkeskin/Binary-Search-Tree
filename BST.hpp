40e4f5c2b92f8190de74b176ea3fd3655fbc5df9
tarikkeskin <60899740+tarikkeskin@users.noreply.github.com>
2020-02-10 23:49:43 +0300

    Add files via upload

--- BST.hpp
+++ BST.hpp
@@ -21,9 +21,24 @@ public: // DO NOT CHANGE THIS PART.
     void printInorder();
 
     BST<T> &operator=(const BST<T> &rhs);
+    
 
 private: // YOU MAY ADD YOUR OWN UTILITY MEMBER FUNCTIONS HERE.
     void printInorder(BSTNode<T> *node);
+    void copyhelper(BSTNode<T>* &to,const BSTNode<T> *obje) const;
+    void inserthelper(BSTNode<T>* &r,const T &data);
+    BSTNode<T> *findhelper(BSTNode<T>* &r,const T &data);
+    void equalhelper(BSTNode<T>* n1);
+    T getmin(BSTNode<T>* r){
+          if (r->left == NULL)
+              return r->data;
+          else
+              return getmin(r->left);
+        }
+    void removehelper(BSTNode<T>* &r,const T &data);
+    void removeall(BSTNode<T>* r);
+
+
 
 private: // DO NOT CHANGE THIS PART.
     BSTNode<T> *root;
@@ -33,36 +48,135 @@ private: // DO NOT CHANGE THIS PART.
 
 template <class T>
 BST<T>::BST() {
+    
+    //root=new BSTNode<T>;
+    root=NULL;
+    //root -> left= NULL;
+    //root -> right=NULL; //root=NULL?????
+
+
     /* TODO */
 }
+template <class T>
+void BST<T>::copyhelper(BSTNode<T>* &to,const BSTNode<T> *obje) const //helperrrr
+{
+    if(obje==NULL)
+    {
+        to=NULL;
+    }
+    else
+    {
+        to = new BSTNode<T>;
+        to->data = obje->data;
+        copyhelper(to->left,obje->left);
+        copyhelper(to->right,obje->right);
+        //copyhelper(to->root,obje->root);
+    }
+}
+    
 
 template <class T>
 BST<T>::BST(const BST<T> &obj) {
-    /* TODO */
-}
 
+    copyhelper(this->root,obj.root);
+   
+}
+template <class T>
+void BST<T>::removeall(BSTNode<T>* r)
+{
+    if(r)
+    {
+        removeall(r->right);
+        removeall(r->left);
+        delete r;
+        r=NULL;
+    }
+}
 template <class T>
 BST<T>::~BST() {
+    
+    removeall(root);
+
     /* TODO */
 }
 
 template <class T>
 BSTNode<T> *BST<T>::getRoot() {
-    /* TODO */
-}
 
+    if(root==NULL) return NULL;
+    return root;
+   
+}
+template<class T>
+void BST<T>::inserthelper(BSTNode<T>* &r,const T &data) //helperrrr
+{
+    if(r==NULL)
+    {
+        r=new BSTNode<T>;
+        r->data=data;
+        
+    }
+    else if(r->data == data) return;
+    else if(r->data < data){
+        if(r->right != NULL) inserthelper(r->right,data);
+        else r->right=new BSTNode<T>(data,NULL,NULL);
+                            }
+    else if(r->data > data){
+        if(r->left!=NULL) inserthelper(r->left,data);
+        else r->left=new BSTNode<T>(data,NULL,NULL);
+                            }
+    else{
+        return;
+}
+    
+}
 template <class T>
 void BST<T>::insert(const T &data) {
+
+    inserthelper(this->root,data);
+
     /* TODO */
 }
-
+template <class T>
+void BST<T>::removehelper(BSTNode<T>* &r,const T &data)
+{
+    if(r==NULL) return;
+    else if(r->data < data) removehelper(r->right,data);
+    else if(r->data > data) removehelper(r->left,data);
+    else
+    {
+        if(r->left != NULL && r->right != NULL){
+            r->data=getmin(r->right);
+            removehelper(r->right,r->data);
+            }
+        else{
+            BSTNode<T>* temp=r;
+            if(r->left!=NULL) r=r->left;
+            else r=r->right;
+            delete temp;
+            temp=NULL;
+            }
+    }
+}
 template <class T>
 void BST<T>::remove(const T &data) {
+    removehelper(root,data);
+
     /* TODO */
+}
+template<class T>
+BSTNode<T> *BST<T>::findhelper(BSTNode<T>* &r,const T &data){
+    if(r==NULL) return NULL;
+    else if(r->data==data) return r;
+    else if(r->data > data) findhelper(r->left,data); //sonrdan return ekledim
+    else if(r->data < data) findhelper(r->right,data); //sonradan return ekledim
+    else return NULL;
+
 }
 
 template <class T>
 BSTNode<T> *BST<T>::find(const T &data) {
+    findhelper(this->root,data); // ** sonradan return ekledim
     /* TODO */
 }
 
@@ -94,8 +208,26 @@ void BST<T>::printInorder(BSTNode<T> *node) {
     }
     printInorder(node->right);
 }
-
+template<class T>
+void BST<T>::equalhelper(BSTNode<T> *n1)
+{
+    if(n1 != NULL)
+    {
+        this->insert(n1->data);
+        equalhelper(n1->left);
+        equalhelper(n1->right);
+    }
+}
 template <class T>
 BST<T> &BST<T>::operator=(const BST<T> &rhs) {
+
+    if(this != &rhs)
+    {
+        removeall(root);
+        BSTNode<T> *curr=rhs.root;
+        equalhelper(curr);
+    }
+    return *this;
+
     /* TODO */
 }
